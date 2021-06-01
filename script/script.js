250ea84014c4ab410d880e03b1a4c4d9fa92187e
ffuad13 <jhonatom99@gmail.com>
2021-06-01 16:49:37 +0800

    add function for project slide

--- script/script.js
+++ script/script.js
@@ -12,3 +12,35 @@ $(document).ready(() => {
     $(".menu-btn i").toggleClass("active");
   });
 });
+
+// project slide =============
+var projectIndex = 1;
+showProject(projectIndex);
+
+function plus(n) {
+  showProject((projectIndex += n));
+}
+
+function currentProject(n) {
+  showProject((projectIndex = n));
+}
+
+function showProject(n) {
+  var i;
+  var project = document.getElementsByClassName("myProject");
+  var dots = document.getElementsByClassName("dot");
+  if (n > project.length) {
+    projectIndex = 1;
+  }
+  if (n < 1) {
+    projectIndex = project.length;
+  }
+  for (i = 0; i < project.length; i++) {
+    project[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  project[projectIndex - 1].style.display = "block";
+  dots[projectIndex - 1].className += " active";
+}
